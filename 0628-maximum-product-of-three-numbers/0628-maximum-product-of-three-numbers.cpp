f95ace3dd4e084227ac64b093b00b1db435db139
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2026-07-26 15:31:22 +0530

    Time: 0 ms (100%), Space: 31.6 MB (46.18%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -5,30 +5,36 @@ public:
 
         int maxi1 = INT_MIN, maxi2 = INT_MIN, maxi3 = INT_MIN;
 
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
 
-        // for(int i = 0;i<nums.size();i++){
-        //     mini1 = min(mini1 , nums[i]);
+        for(int i = 0;i<nums.size();i++){
+            if(maxi1<=nums[i]){
+                maxi3 = maxi2;
+                maxi2= maxi1;
+                maxi1=nums[i];
+            }
+            else if(nums[i]>=maxi2){
+                maxi3 = maxi2;
+                maxi2= nums[i];
+            }
+            else if(nums[i] >=maxi3){
+                maxi3= nums[i];
+            }
 
-        //     if(mini2>nums[i] && mini1<nums[i]){
-        //         mini2 = nums[i];
-        //     }
-
-        //     maxi1 = max(maxi1,nums[i]);
-
-        //     if(maxi2 > nums[i] && maxi1 > nums[i]){
-        //         maxi2 = nums[i];
-        //     }
-
-        //     if(maxi3 > nums[i] && maxi2 > nums[i]){
-        //         maxi3 = nums[i];
-        //     }
-        // }
+            if(nums[i]<=mini1){
+                mini2= mini1;
+                mini1= nums[i];
+            }
+            else if(nums[i]<=mini2){
+                 mini2= nums[i];
+            }
+            
+        }
         // cout<< maxi1 << " "<< maxi2 << " "<<maxi3<< " "<<mini1<<" "<<mini; 
-        int n = nums.size();
+        // int n = nums.size();
 
-        int p1 = nums[n-1] * nums[0] * nums[1];
-        int p2 = nums[n-1] * nums[n-2] * nums[n-3];
+        int p1 = maxi1*mini1*mini2;
+        int p2 = maxi1*maxi2*maxi3;
         cout<<p1<<" "<<p2;
         return (p1 > p2) ? p1 : p2;
     }
