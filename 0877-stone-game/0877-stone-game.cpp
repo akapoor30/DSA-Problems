3a8ad2828e435c3245f9f497b6eb672fc7ed2d46
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2026-08-02 19:37:56 +0530

    Time: 7 ms (35.23%), Space: 19.7 MB (11.06%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                int pickLeft  = nums[i] - dp[i + 1][j];
+                int pickRight = nums[j] - dp[i][j - 1];
+
+                dp[i][j] = max(pickLeft, pickRight);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+};
