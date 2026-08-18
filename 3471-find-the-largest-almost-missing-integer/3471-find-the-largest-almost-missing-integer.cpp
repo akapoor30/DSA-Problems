c2f734fde93e586c0e8da19ec80d997e360a6d14
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2026-08-18 21:48:48 +0530

    Time: 0 ms (100%), Space: 28.7 MB (92.86%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,45 +1,33 @@
-//[3,9,2,1,7] i = 0, j = 0, k = 3;
-// j = 1 
-// j=2
-// 9->2
-// 3->1
-// 2->3
-// 1->2
-// 7->1
-
-// [3,9,2] 
-// [9,2,1]
-// [2,1,7]
-
-// [0,0]
-
-// k = 2
-
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
+        if(k==nums.size()){
+            return *max_element(nums.begin(),nums.end());
+        }
+        vector<int> count(51,0);
+
+        for(int i = 0;i<nums.size();i++){
+            count[nums[i]]++;
+        }
+
+        if(k==1){
+            for(int i =50;i>=0;i--){
+                if(count[i]==1)return i;
+            }
+            
+            return -1;
+            
+        }
+
         int ans = -1;
-        unordered_map<int,int> mp;
 
-        for(int i =0;i<=nums.size()-k;i++){
-            unordered_set<int> unique;
-            // if(i+k<nums.size())break;
-            for(int j = i;j<i+k;j++){
-               unique.insert(nums[j]);
-            }
-
-            for(auto j : unique){
-                mp[j]++;
-            }
-
+        if(count[nums[0]]==1){
+            ans = max(ans,nums[0]);
         }
 
-        for(auto i:mp){
-            if(i.second==1){
-                cout<<i.first;
-                ans = max(ans,i.first);
-            }
+        if(count[nums[nums.size()-1]]==1){
+            ans = max(ans,nums[nums.size()-1]);
         }
-        return ans;
+        return ans ;
     }
 };
